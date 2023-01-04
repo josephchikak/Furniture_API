95452f39bbe3141899b669fb32201e27e74dda51
raytheboffin <111377374+josephchikak@users.noreply.github.com>
2023-01-04 14:39:56 +0200

    changing port

--- server.js
+++ server.js
@@ -47,4 +47,4 @@ app.post('/create-checkout-session', async (req,res) => {
 })
 
 const PORT = process.env.PORT
-app.listen(process.env.PORT || 3000, console.log(`it is working on ${PORT}`))
+app.listen(PORT|| 3000, console.log(`it is working on ${PORT}`))
